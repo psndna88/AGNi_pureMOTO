4c3c3fb900ed18ea3e3f86788c210295dcb8f6bf
psndna88 <psndna88@gmail.com>
2016-04-21 12:48:17 +0530

    AGNi pureCM msm8916 v1.0 support merlin & osprey

---
+++ build_kernel_osprey.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_CM.sh
+. ~/sabermod_gcc_5.3.sh
+export ARCH=arm
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig osprey_defconfig
+fi
+. $KERNELDIR/.config
+mv .git .git-halt
+echo "Cross-compiling kernel ..."
+cd $KERNELDIR/
+make -j3 || exit 1
+rm -rf $KERNELDIR/BUILT_osprey
+mkdir -p $KERNELDIR/BUILT_osprey/lib/modules
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_osprey/lib/modules/ \;
+echo ""
+echo "Stripping unneeded symbols and debug info from module(s)..."
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_osprey/lib/modules/*
+mkdir -p $KERNELDIR/BUILT_osprey/lib/modules/pronto
+mv $KERNELDIR/BUILT_osprey/lib/modules/wlan.ko $KERNELDIR/BUILT_osprey/lib/modules/pronto/pronto_wlan.ko
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_osprey/
+echo ""
+echo "Compiling >> dtbtool <<  for device tree image ..."
+cd $KERNELDIR/tools/dtbtool; make
+echo ""
+echo "Generating Device Tree image (dt.img) ..."
+$KERNELDIR/tools/dtbtool/dtbtool -o $KERNELDIR/BUILT_osprey/dt.img -s 2048 -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/dts/ && rm $KERNELDIR/tools/dtbtool/dtbtool
+cd $KERNELDIR/; mv .git-halt .git
+echo ""
+echo "BUILT_osprey is ready."
