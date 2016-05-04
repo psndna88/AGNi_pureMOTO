aecdb7c1effda7c84b311262e66726e9f991d814
psndna88 <psndna88@gmail.com>
2016-05-04 17:22:08 +0530

    drivers/cpufreq: Implement smartmax cpu governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -434,6 +434,9 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
