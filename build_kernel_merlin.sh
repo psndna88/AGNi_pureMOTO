d5dc89412fcc59c875864eaff202be74e4cec092
psndna88 <psndna88@gmail.com>
2016-05-18 17:52:36 +0530

    Support GCC 6.0 and switch to UBERTC gcc-6.0-arm-eabi

--- build_kernel_merlin.sh
+++ build_kernel_merlin.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
 . ~/AGNi_stamp_MOTO.sh
-. ~/gcc-linaro-5.3-2016.02_armv8l-gnueabihf.sh
+#. ~/gcc-linaro-5.3-2016.02_arm-gnueabi.sh
+#. ~/gcc-5.3-uber_arm-eabi.sh
+. ~/gcc-6.0-uber_arm-eabi.sh
 export ARCH=arm
 if [ ! -f $KERNELDIR/.config ];
 then
